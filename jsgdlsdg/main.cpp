8c45744ef441009ad670b3cf39043c77a095476c
JLongin <neznayupochemu1@gmail.com>
2022-03-31 16:33:41 +0300

    Initital commit of all files to the repository

--- jsgdlsdg/main.cpp
+++ jsgdlsdg/main.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    cout << "Hello world" << endl;
+    if (cout.fail())
+        return EXIT_FAILURE;
+    return EXIT_SUCCESS;
 }
